90b82f7fad75d77f13766787fa42db5fe0cf0009
FARHANATHU SAQTHIYA S <152422349+farhana-8@users.noreply.github.com>
2025-12-30 14:16:07 +0530

    Delete energy-backend directory

--- energy-backend/src/main/java/com/smartenergy/energy_backend/repository/EnergyTipRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartenergy.energy_backend.repository;
-
-import com.smartenergy.energy_backend.model.EnergyTip;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnergyTipRepository extends JpaRepository<EnergyTip, Long> {
-}
